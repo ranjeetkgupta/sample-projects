c80fb3daec5bc22bcf2c042f3ea64b2a6144f4b9
ranjeetkgupta <ranjeetkgupta@gmail.com>
2015-06-29 12:49:23 +0530

    added Tests

---
+++ src/main/java/hello/DataUtility.java
@@ -0,0 +1,41 @@
+package hello;
+
+public class DataUtility {
+	
+	
+	public static void sayA()
+	{
+		
+		System.out.println("A");
+	}
+	
+	
+	public static void sayB()
+	{
+		
+		System.out.println("B");
+	}
+	
+	
+	
+	public static void sayC()
+	{
+		
+		System.out.println("C");
+	}
+	
+	
+	public static void sayD()
+	{
+		
+		System.out.println("D");
+	}
+	
+	
+	public static void sayE()
+	{
+		
+		System.out.println("E");
+	}
+
+}
---
+++ src/test/java/hello/DataUtilityTests.java
@@ -0,0 +1,49 @@
+package hello;
+
+import junit.framework.TestCase;
+
+public class DataUtilityTests extends TestCase {
+	
+	
+	
+	public void test1()
+	{
+		DataUtility.sayA();
+		assertTrue(true);
+		
+	}
+
+	
+	public void test2()
+	{
+		DataUtility.sayB();		
+		assertTrue(true);
+		
+	}
+	
+	
+	public void tes3()
+	{
+		DataUtility.sayC();		
+		assertTrue(true);
+		
+	}
+	
+	
+	public void test4()
+	{
+		DataUtility.sayC();		
+		assertTrue(true);
+		
+	}
+	
+	
+	public void test5()
+	{
+		
+		assertTrue(true);
+		
+	}
+	
+	
+}
